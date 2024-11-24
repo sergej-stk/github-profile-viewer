092c160d8645b266e9db22e6f3390426b50d2fc4
Sergej Steinsiek <sergejsteinsiek@proton.me>
2024-11-24 16:09:57 +0100

    Github Api Caöö

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 A cross-platform desktop app built with .NET and Avalonia UI to view GitHub profiles. It displays key information like repositories, followers, and contributions in a modern, responsive interface.
 
 ## Uses
+
+- [Community Toolkit](https://github.com/CommunityToolkit/dotnet)
 - [Material.Avalonia](https://github.com/AvaloniaCommunity/Material.Avalonia)
+- [Material.Icons](https://github.com/SKProCH/Material.Icons)
+- [Json.net](https://www.newtonsoft.com/json)
 
 ## Authors
 
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,42 @@
-﻿namespace github_profile_viewer.ViewModels;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Newtonsoft.Json;
+
+namespace github_profile_viewer.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    private readonly HttpClient _httpClient = new();
+
+    public async Task<GitHubProfile?> GetGitHubProfile(string username)
+    {
+        var url = $"https://api.github.com/users/{username}";
+        _httpClient.DefaultRequestHeaders.UserAgent.TryParseAdd("request");
+
+        var response = await _httpClient.GetStringAsync(url);
+        var profile = JsonConvert.DeserializeObject<GitHubProfile>(response);
+        return profile;
+    }
+
+    [ObservableProperty] private string _username = String.Empty;
+    
+    [RelayCommand]
+    private async Task Search()
+    {
+        var result = await GetGitHubProfile(Username);
+        Console.Write(result.Login);
+    }   
+}
+
+public class GitHubProfile
+{
+    public string? Login { get; set; }
+    public string? Name { get; set; }
+    public string? AvatarUrl { get; set; }
+    public string? Bio { get; set; }
+    public int? PublicRepos { get; set; }
+    public int? Followers { get; set; }
 }
